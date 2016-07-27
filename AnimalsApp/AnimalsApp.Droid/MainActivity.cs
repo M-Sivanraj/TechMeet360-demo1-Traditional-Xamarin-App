9e6958a4b6014f077208866c722191ebc166cfea
Sivanraj M <m.sivanraj@gmail.com>
2016-07-27 12:57:57 +0530

    Add project files.

---
+++ AnimalsApp/AnimalsApp.Droid/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Linq;
+
+namespace AnimalsApp.Droid
+{
+    [Activity(Label = "AnimalsApp.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        AnimalsViewModel viewModel = new AnimalsViewModel();
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            var list = FindViewById<ListView>(Resource.Id.listView1);
+
+            button.Click += async delegate
+            {
+                button.Enabled = false;
+
+                await viewModel.GetAnimals();
+
+                list.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1,
+                    Android.Resource.Id.Text1, viewModel.Animals.Select(a => $"{a.ID} - from {a.Name}").ToArray());
+
+                button.Enabled = true;
+
+            };
+        }
+    }
+}
+
+
---
+++ AnimalsApp/AnimalsApp/MyClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace AnimalsApp
+{
+
+    public class Animal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class AnimalsViewModel
+    {
+        public ObservableCollection<Animal> Animals { get; } = new ObservableCollection<Animal>();
+
+        public async Task GetAnimals()
+        {
+            try
+            {
+                var client = new HttpClient();
+                var json = await client.GetStringAsync("http://testapiapp360.azurewebsites.net/api/values");
+                var Items = JsonConvert.DeserializeObject<List<Animal>>(json);
+                foreach (var item in Items)
+                {
+                    Animals.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+
+}
+
